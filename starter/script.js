d89d0dc6be8f9261e45404fdb0f635ed45d2d79a
Gregory Oghenah <kenny@gregorys-mbp.home>
2023-12-19 15:03:42 +0000

    Initialized the Event Listener for API Call and added the URL for search

--- starter/script.js
+++ starter/script.js
@@ -1,12 +1,22 @@
+// Variable initialized for User City Input
 let cityName
 
+// Day.js code to get current date
 const now = dayjs();
 const dateNow = now.format('DD/MM/YYYY');
 
+// Event Listener to get User input for City
 $("#search-input").on("keyup", function (e) {
     e.preventDefault();
     cityName = $("#search-input").val();
-
-    console.log(cityName);
     return cityName;    
+});
+
+//Event Listener on the search button for City and API Call
+$("#search-button").on("click", function (event) {
+    event.preventDefault();
+
+    // URL for API Call
+    const queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}&units=metric`;
+    const queryURL1 = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
 });
